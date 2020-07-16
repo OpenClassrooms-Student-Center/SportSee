53c58efa935629db1ea85642eb56dc70dbe91bbc
tdimnet <thomas.dimnet@gmail.com>
2020-07-16 09:14:41 +0200

    Add middleware and new data charts.

--- app/models.js
+++ app/models.js
@@ -1,15 +1,18 @@
 const {
     USER_MAIN_DATA,
-    USER_LAST_SESSIONS
+    USER_ACTIVITY,
+    USER_AVERAGE_SESSIONS
 } = require('./data')
 
 const getUserById = id => USER_MAIN_DATA.filter(user => user.id === id).shift()
 
-const getUserLastSession = userId => {
-    return USER_LAST_SESSIONS.filter(session => session.userId === userId)
-}
+const getUserActivityById = id => USER_ACTIVITY.filter(userActivity => userActivity.userId === id).shift()
+
+
+const getUserAverageSession = id => USER_AVERAGE_SESSIONS.filter(userActivity => userActivity.userId === id).shift()
 
 module.exports = {
     getUserById,
-    getUserLastSession
+    getUserActivityById,
+    getUserAverageSession
 }
