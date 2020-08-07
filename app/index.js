0447d418f94533b0404393e6917e81a870a5410f
tdimnet <thomas.dimnet@gmail.com>
2020-08-07 09:20:02 +0200

    Add use CORS.

--- app/index.js
+++ app/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 
 const router = require('./routes')
 
 const app = express()
+app.use(cors())
 const port = 3000
 
 app.use(router)
