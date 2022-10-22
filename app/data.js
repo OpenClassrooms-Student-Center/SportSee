303020baa9007d3a37759dc9c41888eccaf98908
Sviosdi Ns <sviosdi@yandex.ru>
2022-10-22 08:00:30 +0200

    Update data.js
    champ score de l'utilisateur 12 renommé en score et non todayScore

--- app/data.js
+++ app/data.js
@@ -6,7 +6,7 @@ const USER_MAIN_DATA = [
             lastName: 'Dovineau',
             age: 31,
         },
-        todayScore: 0.12,
+        score: 0.12,
         keyData: {
             calorieCount: 1930,
             proteinCount: 155,
